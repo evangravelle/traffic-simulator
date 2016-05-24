c2e9275d12e3e829fd1d57e643f7eb6462cfc8a1
Julio Martinez <joules_main@julios-mbp-2.attlocal.net>
2016-05-24 12:59:24 -0700

    made changes

--- drawAllVehicles.m
+++ drawAllVehicles.m
@@ -7,9 +7,6 @@ function[vehicle]=drawAllVehicles(inters, vehicle, road, lane, time, t, false)
         in_que = length(vehicle); 
     end
     
-    %number of new Vehicles Spawned
-    spawned = length(road); 
-
     % Draw all old vehicles and current time
     if in_que > 0;
         for i = 1:in_que
@@ -19,6 +16,9 @@ function[vehicle]=drawAllVehicles(inters, vehicle, road, lane, time, t, false)
     
     % Now assign and draw new vehicles
     if isnan(road) == false
+        % number of new Vehicles Spawned
+        spawned = length(road);
+        % make assignments and draw
         for j = 1:spawned
             [vehicle] = makeVehicle(inters,vehicle, in_que + j, lane(j), road(j), time, false);
             vehicle(in_que+j).figure = drawVehicle(vehicle(in_que+j), t);
